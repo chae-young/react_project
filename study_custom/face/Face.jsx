70bbc2cb7688375eacd342b187a0485ae71bde48
chae-young <cycy8527@gmail.com>
2021-03-17 14:53:26 +0900

    class_completed

--- study_custom/face/Face.jsx
+++ study_custom/face/Face.jsx
@@ -4,48 +4,76 @@ import bgImg from './images/face.jpg'
 
 const x = 180;
 const y = 245;
-let count = 0;
+
+const FaceObj = ['Oval','Circl','Oblong','Heart','Square','Triangle'];
 
 class Face extends Component{
 
     state={
         boxCoordX:0,
         boxCoordY:0,
-        result:''
+        result:'',
+        count:0,
     }
 
     interval;
     componentDidMount(){
-        setInterval(()=>{
-            if(this.state.boxCoordX === 0){
-                count++
+        console.log(this.state.count) 
+        this.interval = setInterval(this.interactiveCheck,1000)
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.interval)
+    }
+
+    interactiveCheck = () =>{     
+        const {boxCoordX,boxCoordY,count} = this.state;
+
+        if(boxCoordX === 0){
+            this.setState( (prevState)=>({
+                boxCoordX:x,
+                count:prevState.count+1
+            }))  
+          //  console.log(this.state.count)             
+        }else if(boxCoordX === x){
+            this.setState( (prevState)=>({
+                boxCoordX:x*2,
+                count:prevState.count+1
+            }))  
+           // console.log(this.state.count)   
+        }else if(boxCoordX === x*2){
+            this.setState( (prevState)=>({
+                boxCoordX:0,
+                count:prevState.count+1
+            })) 
+                  
+            if(count === 2){
                 this.setState({
-                    boxCoordX:x,
-                })               
-            }else if(this.state.boxCoordX === x){
-                count++
+                    boxCoordY:y
+                })                    
+            } 
+            if(count === 5){
                 this.setState({
-                    boxCoordX:x*2
-                }) 
-            }else if(this.state.boxCoordX === x*2){
-                count++
-                this.setState({
-                    boxCoordX:0
-                })
-                if(count === 3){
-                    this.setState({
-                        boxCoordY:y
-                    })                    
-                }
-                if(count === 6){
-                    this.setState({
-                        boxCoordX:0,                        
-                        boxCoordY:0
-                    }) 
-                    count=0;                  
-                }                
-            }
-        },1000)
+                    boxCoordX:0,
+                    boxCoordY:0,
+                    count:0
+                })                    
+            }  
+            //console.log(this.state.count)                        
+        }        
+    }
+
+    onClickStop = () =>{
+        clearInterval(this.interval)
+        this.setState({
+            status:true,
+            result:FaceObj[this.state.count]
+        })
+    }
+
+    onClickReplay = () =>{
+        this.interval = setInterval(this.interactiveCheck,1000)
+        
     }
 
     render(){
@@ -61,8 +89,10 @@ class Face extends Component{
                 <h1>얼굴형 선택하기</h1>
                 <div className="box-wrap" style={style}>
                     <div className='box' style={ {left:this.state.boxCoordX,top:this.state.boxCoordY } }></div>
-                    {this.state.result}
                 </div>
+                <button onClick={this.onClickStop}>STOP</button>
+                {this.state.status && <button onClick={this.onClickReplay}>REPLAY</button>}
+                <div>{this.state.result}</div>
             </>
         )
     }
