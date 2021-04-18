097013ce8ca3b85ee6444b07f8be0bde56c9b9fb
chae-young <cycy8527@gmail.com>
2021-04-19 04:59:44 +0900

    completed

--- todoList/TodoList.jsx
+++ todoList/TodoList.jsx
@@ -1,25 +1,24 @@
-import React,{useCallback, useState, useRef} from 'react';
+import React,{useCallback, useState} from 'react';
 import { List, Input, Button, Checkbox } from 'antd';
 import { CloseCircleOutlined } from '@ant-design/icons';
-
-
+import {ListBox} from './styles';
 
 const TodoList = ()=>{
-    const btnRef = useRef();
     const [text,setText] = useState('');
     const [checklist,setCheckList] = useState([]);
-    const [btn,setBtn] = useState([]);
     const [isChecked,setIsChecked] = useState(false);
     
-
     const onChangeText = useCallback((e)=>{
         setText(e.target.value);
     },[])
     const onSubmit = useCallback(()=>{
         if(text!==''){
-           setCheckList([ ...checklist,text])
-        }       
-        
+            setCheckList([ ...checklist,text]);
+            setText('');
+        }     
+    },[text])
+    const handleKeyDown = useCallback((e)=>{
+        onSubmit()
     },[text])
     const onChangeCheckbox = useCallback((e)=>{
         setIsChecked(e.target.checked)
@@ -29,25 +28,30 @@ const TodoList = ()=>{
             return prev.filter(v=>v!==text)
         })
     },[text])
+    const onAllRemoveList = useCallback(()=>{
+        setCheckList([])
+    },[])
 
     return(
-        <>
-            <Input placeholder="나의 할일은.." value={text} onChange={onChangeText}/><Button type="primary" onClick={onSubmit}>Add</Button>
-            <div>
-                {checklist.length ? <List 
+        <div style={{display:'flex',justifyContent:'center'}}>
+            <ListBox>
+                <div className={'input-box'}>
+                    <Input placeholder="나의 할일은.." value={text} onPressEnter={handleKeyDown} onChange={onChangeText}/><Button type="primary" onClick={onSubmit}>Add</Button>
+                </div>
+                {checklist.length ? <div><List 
                     bordered
                     dataSource={checklist}                
                     renderItem={item => (
                         <List.Item>
                             <Checkbox className={isChecked && 'line'} onChange={onChangeCheckbox}>{item}</Checkbox>
-                            <Button ref={btnRef} type="text" onClick={onRemoveList(item)}><CloseCircleOutlined /></Button>
+                            <Button type="text" onClick={onRemoveList(item)}><CloseCircleOutlined /></Button>
                         </List.Item>
                     )}
-                    />: null
-                }
-            </div>
-                       
-        </>
+                    /><Button style={{float:'right'}} type="text" onClick={onAllRemoveList}>Clear</Button></div>
+                : null
+                }         
+            </ListBox>
+        </div>
     )
 }
 
--- todoList/styles.jsx
+++ todoList/styles.jsx
@@ -1,2 +1,17 @@
 import styles from 'styled-components';
+const breakpoint = 767
 
+export const ListBox = styles.div`
+    width:50%;
+
+    & .input-box{
+        display:flex;
+        margin:20px 0;
+    }
+    & .line{
+        text-decoration:line-through;
+    }
+    @media (max-width: ${breakpoint}px) {
+        width:100%;
+    }       
+`
