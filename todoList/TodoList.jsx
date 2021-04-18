50ee4051a5aa10158dcd74620c3df0ee3a5a93b6
chae-young <cycy8527@gmail.com>
2021-04-18 20:45:26 +0900

    modify...

--- todoList/TodoList.jsx
+++ todoList/TodoList.jsx
@@ -2,26 +2,33 @@ import React,{useCallback, useState, useRef} from 'react';
 import { List, Input, Button, Checkbox } from 'antd';
 import { CloseCircleOutlined } from '@ant-design/icons';
 
+let num = 0;
+
 const TodoList = ()=>{
-    const li = useRef('');
+    const btnRef = useRef();
     const [text,setText] = useState('');
     const [checklist,setCheckList] = useState([]);
+    const [btn,setBtn] = useState([]);
     const [isChecked,setIsChecked] = useState(false);
+    
 
     const onChangeText = useCallback((e)=>{
         setText(e.target.value);
     },[])
     const onSubmit = useCallback(()=>{
         if(text!==''){
-           setCheckList([...checklist,text])
-        }
-
+           setCheckList([ ...checklist,text]),
+           setBtn(()=>{
+               return [...btn,num+=1]
+           })
+        }        
     },[text])
     const onChangeCheckbox = useCallback((e)=>{
         setIsChecked(e.target.checked)
     },[])
     const onRemoveList = useCallback((e)=>{
-        console.log(checklist)      
+         const objCheckList = {...checklist}
+        console.log(btn)
     },[text])
 
     return(
@@ -30,12 +37,11 @@ const TodoList = ()=>{
             <div>
                 {checklist.length ? <List 
                     bordered
-                    dataSource={checklist}
+                    dataSource={checklist}                    
                     renderItem={item => (
-                        <List.Item >
-                            { }
-                            <Checkbox className={isChecked && 'line'} onChange={onChangeCheckbox}>{item}</Checkbox>
-                            <Button type="text" onClick={onRemoveList}><CloseCircleOutlined /></Button>
+                        <List.Item>
+                            <Checkbox className={isChecked && 'line'} onChange={onChangeCheckbox}>{}</Checkbox>
+                            <Button ref={btnRef} type="text" onClick={onRemoveList}><CloseCircleOutlined /></Button>
                         </List.Item>
                     )}
                     />: null
