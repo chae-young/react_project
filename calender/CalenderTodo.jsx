16177109ebdbc9325201a880d5767907a60b0b61
chae-young <cycy8527@gmail.com>
2021-04-24 23:12:47 +0900

    week_add

--- calender/CalenderTodo.jsx
+++ calender/CalenderTodo.jsx
@@ -1,14 +1,56 @@
 import React,{useState} from 'react';
-import Calendar from './Calender';
-// import Calendar from 'react-calendar';
-// import 'react-calendar/dist/Calendar.css';
+import moment from 'moment'
+import 'moment/locale/ko';
+import Week from './Week';
 
 const CalenderTodo = ()=>{
-    const [dateVal,setDateVal] = useState(new Date());
-
-
+    const [today,setToday] = useState(moment());
+    const renderWeeks = ()=> {
+        let weeks = [];
+        let done = false;
+        //전달 일요일
+        let date = today.clone().startOf("month").add("w" -1).day("Sunday");
+        let count = 0;
+        //전달
+        let monthIndex = date.month();
+        while (!done) {
+            weeks.push(
+                1
+            );  
+            date.add(1, "w");
+            console.log(monthIndex)
+            done = count++ > 2 && monthIndex !== date.month();
+            monthIndex = date.month();
+         // console.log(count,monthIndex,date.month())
+          
+        }
+    
+        console.log(weeks)
+      };
+      renderWeeks()
     return(
-        <Calendar/>
+        <div>
+            <div className="calender__head">
+                <button></button>{today.format("YYYY[년] MMMM")}<button></button>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Sun</th>
+                            <th>Mon</th>
+                            <th>Tue</th>
+                            <th>Wed</th>
+                            <th>Thu</th>
+                            <th>Fri</th>
+                            <th>Sat</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+            </div>
+            
+        </div>
     )
 }
 
---
+++ calender/Week.jsx
@@ -0,0 +1,10 @@
+import React,{useState} from 'react';
+import moment from 'moment'
+
+const Week = ()=>{
+    return(
+        <div></div>
+    )
+}
+
+export default Week;
