43f9b662fead09333f3c5148091b9ed0ca337c67
chae-young <cycy8527@gmail.com>
2021-12-18 18:22:10 +0900

    fullstack init

---
+++ fullstack/README.md
@@ -0,0 +1,10 @@
+```
+yarn init -y
+
+```
+
+```
+실행
+
+yarn run client
+```
---
+++ fullstack/client/pages/index.js
@@ -0,0 +1,11 @@
+import MsgList from "../components/MsgList";
+
+const Home = () => {
+    return (
+        <>
+            <h1>hi</h1>
+            <MsgList />
+        </>
+    );
+};
+export default Home;
