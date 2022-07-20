09ca58d119a2c725c5e7aba185ba8553bd29cfa3
이챙 <cycy8527@gmail.com>
2022-07-20 11:17:46 +0900

    프론트 완성

--- fullstack/client/components/MsgList.js
+++ fullstack/client/components/MsgList.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import MsgInput from "./MsgInput"
 import MsgItem from "./MsgItem"
 
 const UserIds = ['채영',"수지","나은"]
@@ -22,16 +23,63 @@ const msgs = Array(5).fill(0).map((_, i) => ({
 // ]
 
 const MsgList = () => {
-    const [msgArr,setMsgArr] = useState() 
+    const [originalMsgArr,originalSetMsgArr] = useState() 
+    const [editingId,setEditingId] = useState(null)
 
     useEffect(()=>{
-        setMsgArr(msgs)
+        originalSetMsgArr(msgs)
     },[])
 
+    const onCreate = text => {
+        console.log(text)
+        const newMsg = {
+            id: originalMsgArr.length + 1,
+            userId: getRandomUserId(),
+            timestamp: Date.now(),
+            text: `${originalMsgArr.length + 1} text`
+        }
+        originalSetMsgArr(msg => [newMsg,...msg])
+    }
+
+    const onUpdate = (text,id) => {
+        originalSetMsgArr(msgs => {
+            const targetIndex = msgs.findIndex(msg => msg.id === id)// 기존 msg로 데이터 가져와서 안정적..?
+            if(targetIndex < 0) return msgs
+            const newMsgs = [...msgs]
+            //새로운 값 추가
+            newMsgs.splice(targetIndex,1,{
+                ...msgs[targetIndex],
+                text
+            })
+
+            return newMsgs
+        })
+        doneEdit()
+    }
+
+    const doneEdit = () => setEditingId(null)
+
+    const onDelete = (id) => {
+        const deletMsg = originalMsgArr.filter((msg,i) => msg.id !== id)
+        originalSetMsgArr(deletMsg)
+    } 
+
     return(
-    <ul className="messages">
-        {msgArr?.map(x => <MsgItem key={x.id} {...x}/>)}
-    </ul>    
+    <>
+        <MsgInput mutate={onCreate}/>
+        <ul className="messages">
+            {originalMsgArr?.map(x => 
+                <MsgItem 
+                    key={x.id} 
+                    {...x} 
+                    onUpdate={onUpdate}
+                    onDelete={() => onDelete(x.id)}
+                    startEdit={() => setEditingId(x.id)}
+                    isEditing={editingId === x.id}
+                />
+            )}
+        </ul>    
+    </>
     )
 }
 
