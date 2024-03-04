88ae7e29e6ab082441737eabbdc344d0cf7b9df8
chae-young <cycy8527@gmail.com>
2024-03-04 13:56:33 +0900

    Fix: 번들 검색과 코드 스플리팅 적용

--- lecture-1-master/src/App.js
+++ lecture-1-master/src/App.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import './App.css'
-import ListPage from './pages/ListPage/index'
-import ViewPage from './pages/ViewPage/index'
+// import ListPage from './pages/ListPage/index'
+// import ViewPage from './pages/ViewPage/index'
+
+const ListPage = lazy(() => import('./pages/ListPage'))
+const ViewPage = lazy(() => import('./pages/ViewPage'))
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route path="/" component={ListPage} exact />
-        <Route path="/view/:id" component={ViewPage} exact />
-      </Switch>
+      <Suspense fallback={<div>로딩즁...</div>}>
+        <Switch>
+          <Route path="/" component={ListPage} exact />
+          <Route path="/view/:id" component={ViewPage} exact />
+        </Switch>
+      </Suspense>
     </div>
   )
 }
