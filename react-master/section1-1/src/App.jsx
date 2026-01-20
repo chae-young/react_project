25743046d6bd79eeaffa197e325727fc253b5a36
이챙 <cycy8527@gmail.com>
2026-01-20 14:51:55 +0900

    Update: section1-01 useReducer는 왜 필요한가?

---
+++ react-master/section1-1/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import CartPage from './pages/CartPage'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <CartPage/>
+      </div>
+    </>
+  )
+}
+
+export default App
