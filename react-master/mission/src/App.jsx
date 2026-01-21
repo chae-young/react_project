41dc85c9d7be2d17d12d2241df0db4b272277417
이챙 <cycy8527@gmail.com>
2026-01-21 09:46:27 +0900

    Update: mission3

--- react-master/mission/src/App.jsx
+++ react-master/mission/src/App.jsx
@@ -1,12 +1,13 @@
 import Cafeteria from "./components/Cafeteria"
 import ShoppingCart from "./components/ShoppingCart"
+import SmartTicket from "./components/SmartTicket"
 
 function App() {
 
   return (
     <>
       <div>
-        <ShoppingCart />
+        <SmartTicket />
       </div>
     </>
   )
